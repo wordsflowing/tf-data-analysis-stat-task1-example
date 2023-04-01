f16ed47cef9b1b0bfc5ac6600b3e181bab5ddc19
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-01 11:46:59 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
